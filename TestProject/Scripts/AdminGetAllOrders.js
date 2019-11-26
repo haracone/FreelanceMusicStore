c72a80f2717289b05f4cccdba5427f84bb83724f
art.zibarev1@gmail.com <art.zibarev1@gmail.com>
2019-11-26 03:42:30 -0800

    Added knockout filter

--- TestProject/Scripts/AdminGetAllOrders.js
+++ TestProject/Scripts/AdminGetAllOrders.js
@@ -1,17 +1,35 @@
-﻿   var Orders = {
-        OrdersCollection: ko.observableArray(),
-};
+﻿/*var Orders = {
+    OrdersCollection: ko.observableArray(),
+    Filters: ko.observable()
+};*/
 
-    $(document).ready(function () {
-        $.ajax({
-            type: "GET",
-            url: "/Admin/GetAllOrders",
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-        }).done(function (data) {
-            $(data).each(function (index, element) {
-                Orders.OrdersCollection.push(element);
+function Orders() {
+    var self = this;
+    self.OrdersCollection = ko.observableArray();
+    self.CurrentFilter = ko.observable();
+
+    self.filterOrders= ko.computed(function () {
+        if (!self.CurrentFilter()) {
+            return self.Orders;
+        } else {
+            return ko.utils.arrayFilter(self.Orders(), function (ord) {
+                return ord.ClientId == self.CurrentFilter();
             });
-            ko.applyBindings(Orders);
-        })
+        }
     });
+}
+
+$(document).ready(function () {
+    var Order = new Orders();
+    $.ajax({
+        type: "GET",
+        url: "/Admin/GetAllOrders",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+    }).done(function (data) {
+        $(data).each(function (index, element) {
+            Order.OrdersCollection.push(element);
+        });
+        ko.applyBindings(Order);
+    })
+});
